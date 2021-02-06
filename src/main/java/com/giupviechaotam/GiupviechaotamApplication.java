02872a4cb1857ad125907e1c678b330a77cca60a
cucot <phucnn.work@gmail.com>
2021-02-06 20:26:21 +0700

    new files

---
+++ src/main/java/com/giupviechaotam/GiupviechaotamApplication.java
@@ -0,0 +1,13 @@
+package com.giupviechaotam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiupviechaotamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiupviechaotamApplication.class, args);
+	}
+
+}
