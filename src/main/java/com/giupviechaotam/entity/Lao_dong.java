0e872d142cafd2bd4cbe8efc1a237c13fcbd61d6
cucot <phucnn.work@gmail.com>
2021-03-19 18:00:15 +0700

    add vue.js and h2 DB

--- src/main/java/com/giupviechaotam/entity/Lao_dong.java
+++ src/main/java/com/giupviechaotam/entity/Lao_dong.java
@@ -5,13 +5,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
+@Entity
+@Table(name = "LAO_DONG")
 public class Lao_dong {
 	
 	@Id
---
+++ src/main/resources/static/assets/js/pages/laodong.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+	el: 'div#app',
+	data: {
+		heading: 'Welcome to quan ly lao dong',
+		content: 'Hello, Vue!'
+	}
+})
