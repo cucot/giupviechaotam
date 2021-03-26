51a00c40986216fab71625519b50d08250106da3
cucot <phucnn.work@gmail.com>
2021-03-27 00:54:25 +0700

    - remove bin folder - add logo and navbar

--- src/main/resources/static/assets/js/pages/laodong.js
+++ src/main/resources/static/assets/js/pages/laodong.js
@@ -1,7 +1,18 @@
-var app = new Vue({
-	el: 'div#app',
-	data: {
-		heading: 'Welcome to quan ly lao dong',
-		content: 'Hello, Vue!'
-	}
-})
+const app = new Vue({
+    el: 'div#app',
+    data: {
+        companyName: 'Hao Tam',
+        role: 'Giup viec',
+    	homepage_link: 'https://giupviechaotam.com',
+		laodong_link: 'https://giupviechaotam.com/nguoi-giup-viec/',
+		chunha_link: 'https://giupviechaotam.com/chu-nha/',
+        heading: 'Welcome to quan ly lao dongg',
+        content: 'Hello, Vue!',
+        description: 'This is the very first day with Vue.js'
+    },
+    computed: {
+        fullTitle() {
+            return this.role + " " + this.companyName;
+        }
+    }
+});
