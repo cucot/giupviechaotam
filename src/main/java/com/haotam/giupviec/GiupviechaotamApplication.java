f64837685c4f0d018e1021086eff55aa5ebb5b8e
cucot <phucnn.work@gmail.com>
2021-04-01 23:27:37 +0700

    decided to use pure html css js and thymeleaf as template engine

--- src/main/java/com/haotam/giupviec/GiupviechaotamApplication.java
+++ src/main/java/com/haotam/giupviec/GiupviechaotamApplication.java
@@ -2,15 +2,29 @@ package com.haotam.giupviec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = "com.haotam.giupviec")
 public class GiupviechaotamApplication {
 
 	public static void main(String[] args) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		System.out.println(encoder.encode("123456"));
+//		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//		System.out.println(encoder.encode("123456"));
 		SpringApplication.run(GiupviechaotamApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("http://localhost:8080");
+				WebMvcConfigurer.super.addCorsMappings(registry);
+			}
+			
+		};
 	}
 
 }
--- src/main/resources/static/assets/js/pages/laodong.js
+++ src/main/resources/static/assets/js/pages/laodong.js
@@ -1,22 +1,3 @@
-// const app = new Vue({
-//     el: 'div#app',
-//     data: {
-//         companyName: 'Hao Tam',
-//         role: 'Giup viec',
-//     	homepage_link: 'https://giupviechaotam.com',
-// 		laodong_link: 'https://giupviechaotam.com/nguoi-giup-viec/',
-// 		chunha_link: 'https://giupviechaotam.com/chu-nha/',
-//         heading: 'Welcome to quan ly lao dongg',
-//         content: 'Hello, Vue!',
-//         description: 'This is the very first day with Vue.js'
-//     },
-//     computed: {
-//         fullTitle() {
-//             return this.role + " " + this.companyName;
-//         }
-//     }
-// });
-
 $(document).ready(function() {
     const dataTable = $('#tblLaoDong').dataTable({
         serverSide: true,
