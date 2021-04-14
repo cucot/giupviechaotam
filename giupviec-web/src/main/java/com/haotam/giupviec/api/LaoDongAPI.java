4124df256eec4b74923e14e80ca957f535f9568e
cucot <phucnn.work@gmail.com>
2021-04-14 23:36:26 +0700

    - split project into multi modules project. Closes #1

--- giupviec-web/src/main/java/com/haotam/giupviec/api/LaoDongAPI.java
+++ giupviec-web/src/main/java/com/haotam/giupviec/api/LaoDongAPI.java
@@ -1,5 +1,9 @@
 package com.haotam.giupviec.api;
 
+import com.haotam.giupviec.api.request.DataTableRequest;
+import com.haotam.giupviec.api.response.DataTableResponse;
+import com.haotam.giupviec.model.Worker;
+import com.haotam.giupviec.services.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,18 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.haotam.giupviec.api.request.DataTableRequest;
-import com.haotam.giupviec.api.response.DataTableResponse;
-import com.haotam.giupviec.model.Worker;
-import com.haotam.giupviec.services.WorkerService;
-
 @RestController
 @RequestMapping("/api")
 public class LaoDongAPI {
 
     @Autowired
     private WorkerService workerService;
-    
+
     @CrossOrigin
     @RequestMapping(path = "/employees", method = RequestMethod.GET)
     public DataTableResponse<Worker> getAll(@ModelAttribute DataTableRequest dataTableRequest) {
--- giupviec-web/src/main/java/com/haotam/giupviec/repositories/WorkerRepository.java
+++ giupviec-web/src/main/java/com/haotam/giupviec/repositories/WorkerRepository.java
@@ -1,8 +1,8 @@
 package com.haotam.giupviec.repositories;
 
+import com.haotam.giupviec.model.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.haotam.giupviec.model.Worker;
 
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
 }
